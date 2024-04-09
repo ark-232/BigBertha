b42418bb61b4c4121a5a043ec87b6c9c18144c47
ark-232 <arkady232@gmail.com>
2024-04-09 15:08:29 -0600

    Disable ICMP ping responses for both IPv4 and IPv6

--- disbable_ICMP.sh
+++ disbable_ICMP.sh
@@ -8,5 +8,6 @@ iptables -I INPUT -p icmp --icmp-type echo-request -j DROP
 
 #make the rule persistent across reboots on systems using iptables-persistent
 iptables-save > /etc/iptables/rules.v4
+ip6tables -I INPUT -p ipv6-icmp --icmpv6-type echo-request -j DROP
 
 echo "ICMP ping responses have been disabled."
