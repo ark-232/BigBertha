d762ca2adaf698855e2efa7d2ab7f64db98694f3
B00TK1D <stearns.josiah@gmail.com>
2024-04-09 23:18:27 -0400

    Added immutable binary folders

--- josiah/lockdown.sh
+++ josiah/lockdown.sh
@@ -4,4 +4,9 @@ chattr +i /root/.ssh/authorized_keys
 chattr +i /etc/passwd
 chattr +i /etc/shadow
 
+chattr +i /bin
+chattr +i /usr/bin
+chattr +i /root
+chattr +i /etc
+
 mv /usr/bin/chattr /usr/bin/rttahc
